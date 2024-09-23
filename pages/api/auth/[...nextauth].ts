0624343c761e015bc95726e43005c13ebba1e1cc
Milosch Füllgraf <milosch.fuellgraf@testws.de>
2024-09-23 21:26:26 +0200

    small fixes in authentication

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,26 +1,18 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
-import SpotifyProvider from "next-auth/providers/spotify"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import prisma from '../../../prisma/client.ts'
+import prisma from '../../../prisma/client'
+
+console.log(process.env);
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
     secret: process.env.AUTH_SECRET,
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        }),
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-        }),
-        SpotifyProvider({
-            clientId: process.env.SPOTIFY_CLIENT_ID,
-            clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         })
     ],
-}
+};
 export default NextAuth(authOptions)
